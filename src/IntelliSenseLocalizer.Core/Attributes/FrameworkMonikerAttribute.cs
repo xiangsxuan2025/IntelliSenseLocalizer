a93c7d3c33947328923dbb641c75285a027a0920
xiangsxuan2025 <xiangshouxuan@foxmail.com>
2025-11-14 21:21:18 +0800

    chore: 添加注释，便于阅读和重构

--- src/IntelliSenseLocalizer.Core/Attributes/FrameworkMonikerAttribute.cs
+++ src/IntelliSenseLocalizer.Core/Attributes/FrameworkMonikerAttribute.cs
@@ -8,30 +8,53 @@ namespace IntelliSenseLocalizer;
 [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
 public sealed class FrameworkMonikerAttribute : Attribute
 {
+    /// <summary>
+    /// 框架标识符
+    /// 例如：net6.0, netcoreapp3.1 等
+    /// </summary>
     public string Moniker { get; }
 
+    /// <summary>
+    /// 初始化框架标识属性
+    /// </summary>
+    /// <param name="moniker">框架标识符</param>
+    /// <exception cref="ArgumentException">当标识符为空或空白时抛出</exception>
     public FrameworkMonikerAttribute(string moniker)
     {
+        // 验证输入参数
         if (string.IsNullOrWhiteSpace(moniker))
         {
             throw new ArgumentException($"“{nameof(moniker)}”不能为 null 或空白。", nameof(moniker));
         }
-
         Moniker = moniker;
     }
 }
 
+/// <summary>
+/// 框架标识属性扩展方法
+/// 提供从类型和枚举值获取框架标识符的功能
+/// </summary>
 public static class FrameworkMonikerAttributeExtensions
 {
+    /// <summary>
+    /// 从类型获取框架标识符
+    /// </summary>
+    /// <param name="type">目标类型</param>
+    /// <returns>框架标识符，如果未找到返回null</returns>
     public static string? GetFrameworkMoniker(this Type type)
     {
         if (type.GetCustomAttribute<FrameworkMonikerAttribute>() is FrameworkMonikerAttribute frameworkMonikerAttribute)
         {
             return frameworkMonikerAttribute.Moniker;
-        }
+    }
         return null;
     }
 
+    /// <summary>
+    /// 从枚举值获取框架标识符
+    /// </summary>
+    /// <param name="enumValue">枚举值</param>
+    /// <returns>框架标识符，如果未找到返回null</returns>
     public static string? GetFrameworkMoniker(this Enum enumValue)
     {
         if (enumValue.GetCustomAttribute<FrameworkMonikerAttribute>() is FrameworkMonikerAttribute frameworkMonikerAttribute)
--- src/IntelliSenseLocalizer.Core/DefaultIntelliSenseItemProvider.cs
+++ src/IntelliSenseLocalizer.Core/DefaultIntelliSenseItemProvider.cs
@@ -7,23 +7,27 @@ namespace IntelliSenseLocalizer;
 /// <summary>
 /// 默认 IntelliSense 项提供者
 /// 负责从 XML 文档中提取 IntelliSense 项描述符
+/// 实现 IIntelliSenseItemProvider 接口
 /// </summary>
 public class DefaultIntelliSenseItemProvider : IIntelliSenseItemProvider
 {
     /// <summary>
     /// 从 XML 文档中获取所有 IntelliSense 项描述符
+    /// 遍历 XML 文档中的所有 member 节点并创建对应的描述符
     /// </summary>
     /// <param name="xmlDocument">XML 文档对象</param>
     /// <param name="intelliSenseFileDescriptor">IntelliSense 文件描述符</param>
     /// <returns>IntelliSense 项描述符的枚举</returns>
     public IEnumerable<IntelliSenseItemDescriptor> GetItems(XmlDocument xmlDocument, IntelliSenseFileDescriptor intelliSenseFileDescriptor)
     {
-        // 获取所有 member 节点
+        // 获取所有 member 节点，这些节点包含 API 文档信息
         var memberNodeList = xmlDocument.GetElementsByTagName("member");
 
-        // 为每个 member 节点创建描述符
+        // 遍历每个 member 节点并创建描述符
         foreach (XmlElement member in memberNodeList)
         {
+            // 为每个 member 节点创建描述符
+            // 使用 yield return 实现延迟加载，提高内存效率
             yield return IntelliSenseItemDescriptor.Create(member, intelliSenseFileDescriptor);
         }
     }
